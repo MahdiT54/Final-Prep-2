b0dff28c18671abbd0f7a6d3c7664acd83d258ed
MahdiT54 <mahditanzim2@gmail.com>
2023-11-07 22:47:36 -0500

    async/await the react way

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,18 +1,21 @@
-import React, { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 const Posts = () => {
-    const { id } = useParams();
+  const { id } = useParams();
 
-    useEffect(() => {
-        const data = fetch("https://jsonplaceholder.typicode.com/posts/1")
-        console.log(data)
-    }, []);
-    return (
-        <div>
-            {id} {/* dynamic route */}
-        </div>
-    );
-}
+  useEffect(() => {
+    async function fetchPosts() {
+      const data = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+      console.log(data);
+    }
+    fetchPosts();
+  }, []);
+  return (
+    <div>
+      {id} {/* dynamic route */}
+    </div>
+  );
+};
 
 export default Posts;
