4f84965a8d4b824cf7308010403bb0c59a97c3c6
MahdiT54 <mahditanzim2@gmail.com>
2023-11-09 12:40:11 -0500

    posts mapped

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -17,11 +17,30 @@ const Posts = () => {
   }, []);
 
   return (
-    <div>
+    <>
+      <div className="post__search">
+        <button>← Back</button>
+        <div className="post__search--container">
+          <label className="post__search--label">Search by Id</label>
+          <input type="number" />
+          <button>Enter</button>
+        </div>
+      </div>
       {posts.map((post) => (
-        <div>{post.id}</div>
+        <div className="post">
+          <div className="post__title">{post.title}</div>
+          <p className="post__body">{post.body}</p>
+        </div>
       ))}
-    </div>
+      <div className="post">
+        <div className="post__title">
+          <div className="post__title--skeleton"></div>
+        </div>
+        <div className="post__body">
+          <p className="post__body--skeleton"></p>
+        </div>
+      </div>
+    </>
   );
 };
 
