0904b3e2b3e8df12f4392db002edaa0787b3531a
MahdiT54 <mahditanzim2@gmail.com>
2023-11-09 21:24:41 -0500

    home loading state

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,20 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { HomeIcon } from "@heroicons/react/outline";
 
 const Home = () => {
   const navigate = useNavigate();
   const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState();
 
   async function getUsers() {
+    setLoading(true);
     const { data } = await axios.get(
       "https://jsonplaceholder.typicode.com/users"
     );
     setUsers(data);
+    setLoading(false); // executes after fetchPosts() +2 lines down
   }
 
   useEffect(() => {
@@ -21,25 +25,46 @@ const Home = () => {
     <div className="container">
       <div className="row">
         <div className="user-list">
-          {users.map((user) => (
-            <div className="user" key={user.id} onClick={() => navigate(`${user.id}`)}>
-              <div className="user-card">
-                <div className="user-card__container">
-                  <h3>{user.name}</h3>
-                  <p>
-                    <b>Email:</b> {user.email}
-                  </p>
-                  <p>
-                    <b>Phone:</b> {user.phone}
-                  </p>
-                  <p>
-                    <b>Website:</b>
-                    {user.website}
-                  </p>
+          {loading ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="home__load--icon w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
+              />
+            </svg>
+          ) : (
+            users.map((user) => (
+              <div
+                className="user"
+                key={user.id}
+                onClick={() => navigate(`${user.id}`)}
+              >
+                <div className="user-card">
+                  <div className="user-card__container">
+                    <h3>{user.name}</h3>
+                    <p>
+                      <b>Email:</b> {user.email}
+                    </p>
+                    <p>
+                      <b>Phone:</b> {user.phone}
+                    </p>
+                    <p>
+                      <b>Website:</b>
+                      {user.website}
+                    </p>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))
+          )}
         </div>
       </div>
     </div>
